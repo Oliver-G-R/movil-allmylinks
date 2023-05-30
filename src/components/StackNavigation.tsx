a98488ff57952f811dcfdb4e42605a6c684e381a
Oliver <oliver.kali.hub@gmail.com>
2023-05-30 02:01:09 -0600

    add: tipos para la navegacion y ruta login

--- src/components/StackNavigation.tsx
+++ src/components/StackNavigation.tsx
@@ -1,15 +1,17 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { NavigationContainer } from '@react-navigation/native';
 
-import { Home } from "../screens"
+import { Home, Login } from "../screens"
+import { RootStackParamList } from "../types/NavigationType";
 
-const stack = createNativeStackNavigator()
+const stack = createNativeStackNavigator<RootStackParamList>()
 
 export const StackNavigation = () => {
   return (
     <NavigationContainer>
       <stack.Navigator>
         <stack.Screen name="Home" component={Home} />
+        <stack.Screen name="Login" component={Login} />
       </stack.Navigator>
     </NavigationContainer>
   )
